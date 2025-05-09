aa4e0bcb6faac4157c93ac9036cecb8e369051f1
Hung hay ho <hungok2909@gmail.com>
2025-05-09 14:48:01 +0700

    added getUserPostsServicev2 featuring isLiked status of user(optional)

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,8 +1,8 @@
-const { getUserPostsService } = require('../services/profileService');
+const { getUserPostsService, getUserPostsServicev2 } = require('../services/profileService');
 
 const getUserPosts = async (req, res) => {
     const userId = req.params.id;
-    const posts = await getUserPostsService(userId, req.query);
+    const posts = await getUserPostsServicev2(userId, req.query);
 
     res.status(200).json({
         errorCode: 0,
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
 const aqp = require('api-query-params');
 const likeServicev3 = require('./likeServicev3');
+const Like = require('../models/likev2');
 
 const getUserPostsService = async (userId, query) => {
     const page = parseInt(query.page) || 1;
@@ -22,4 +23,42 @@ const getUserPostsService = async (userId, query) => {
     return posts;
 };
 
-module.exports = { getUserPostsService };
+const getUserPostsServicev2 = async (authorId, query) => {
+    const page = parseInt(query.page) || 1;
+    const userId = query.userId;
+
+    const { limit = 10, population } = aqp(query);
+    const offset = (page - 1) * limit;
+
+    const posts = await Post.find({ author: authorId })
+        .populate(population || "author")
+        .sort({ createdAt: -1 })
+        .skip(offset)
+        .limit(limit)
+        .lean();
+
+    // Gán likeCount cho từng post
+    for (let post of posts) {
+        post.likeCount = (await likeServicev3.countLikesService(post._id, 'post')).likes;
+    }
+
+    if (userId == null || userId == undefined) return posts;
+
+    const likedPostIds = await Like.find({
+        user: userId,
+        target: { $in: posts.map(p => p._id) },
+        onModel: 'post'
+    }).distinct('target');
+
+    const likedPostIdStrings = likedPostIds.map(id => id.toString());
+
+    // Gắn isLiked vào từng bài viết
+    const postsWithLikeStatus = posts.map(post => ({
+        ...post,
+        isLiked: likedPostIdStrings.includes(post._id.toString())
+    }));
+
+    return postsWithLikeStatus;
+};
+
+module.exports = { getUserPostsService, getUserPostsServicev2 };
